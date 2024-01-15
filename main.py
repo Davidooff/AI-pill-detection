7676dea43de1e7bcf0caa6f2687202b2413a9d2f
Davidooff <david.ter.18@gmail.com>
2024-01-15 00:09:44 -0500

    :)

--- main.py
+++ main.py
@@ -34,16 +34,19 @@ def load_data_set(type='train'):
     temp = list(map(lambda x: int(x)/default_img_shape if x.isnumeric() else x, raws[index]))
     path_to_img = os.path.join(IMG_DIR_PATH, temp[0]) 
     image = cv.imread(path_to_img, cv.IMREAD_GRAYSCALE)
-    out_data["images"].append(image)
+    image = cv.resize(image, (256,256))
+    out_data["images"].append(image / 255.0)
     out_data["targets"].append(temp[1::])
 
+  out_data["images"] = np.array(out_data["images"], dtype="float32") 
+  out_data["targets"] = np.array(out_data["targets"], dtype="float32")
   return out_data
 
 def init_model(train_img, train_target, test_img, test_target): #predictor):
   model = keras.Sequential()
 
   # Block 1
-  model.add(keras.layers.Conv2D(filters = 64, kernel_size = 3, padding="same", input_shape = (default_img_shape, default_img_shape, 1), activation= "relu"))
+  model.add(keras.layers.Conv2D(filters = 64, kernel_size = 3, padding="same", input_shape = (256, 256, 1), activation= "relu"))
   model.add(keras.layers.Conv2D(filters = 64, kernel_size = 3, padding="same", activation= "relu"))
   model.add(keras.layers.MaxPooling2D())
   
@@ -72,6 +75,7 @@ def init_model(train_img, train_target, test_img, test_target): #predictor):
   model.add(keras.layers.MaxPooling2D())
   
   # Top
+
   model.add(keras.layers.Flatten())
   model.add(keras.layers.Dense(128, activation="relu"))
   model.add(keras.layers.Dense(64, activation="relu"))
@@ -83,7 +87,7 @@ def init_model(train_img, train_target, test_img, test_target): #predictor):
 
   opt = keras.optimizers.Adam()
   loss_fn = keras.losses.binary_crossentropy
-  model.compile(loss=loss_fn, optimizer=opt)
+  model.compile(loss="mse", optimizer=opt)
   # print(model.summary())
 
   H = model.fit(train_img, train_target, validation_data=(test_img, test_target), batch_size=32, epochs=5, verbose=1)
@@ -103,5 +107,5 @@ def init_model(train_img, train_target, test_img, test_target): #predictor):
 
 train_data = load_data_set()
 test_data = load_data_set("test")
-
+# print(test_data["images"][1])
 init_model(train_data["images"], train_data["targets"], test_data["images"], test_data["targets"])
